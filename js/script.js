d24d35fda191670fb2d49911ec27140154ac6e7a
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-27 09:23:17 -0300

    feito todo o comentário do código

--- js/script.js
+++ js/script.js
@@ -7,15 +7,20 @@ const btn_resultado = document.querySelector(".btn_resultado")
 
 const resultado = document.querySelector(".resultado")
 
+// ARRAY QUE ARMAZENA OS BOTÕES CLICADOS.
 let array_conteudo = []
 
+// ARRAY QUE ARMAZENA O "dataset" DE CADA BOTÃO CLICADO.
 let array_data = []
 
+// CONTROLADOR DOS BOTÕES DE OPERAÇÃO.
 let sinal = false
 
+// VERIFICA QUAL BOTÃO FOI CLICADO, INSERE SEU VALOR NO VISOR E FAZ UM PUSH DESTE VALOR E SEU DATASET EM SEUS RESPECTIVOS ARRAYS.
 botoes.map((btn)=>{
     btn.addEventListener("click",(elemento)=>{
 
+        // RETORNA O SINAL PRA FALSE PARA SER POSSÍVEL DIGITAR OPERADORES NOVAMENTE.
         sinal = false
 
         visor.innerHTML += elemento.target.innerHTML
@@ -25,10 +30,14 @@ botoes.map((btn)=>{
     })
 })
 
+// VERIFICA QUAL BOTÃO FOI CLICADO, INSERE SEU VALOR NO VISOR E FAZ UM PUSH DESTE VALOR E SEU DATASET EM SEUS RESPECTIVOS ARRAYS.
 btn_operador.map((btn_op)=>{
     btn_op.addEventListener("click",(elemento)=>{
+
+        // O IF FAZ UMA CHECAGEM NO CONTROLADOR E, SE ESTIVER FALSE, PERMITE DIGITAR UM OPERADOR.
         if(!sinal){
 
+            // QUANDO O OPERADOR FOR DIGITADO, MUDA O CONTROLADOR PARA TRUE E BLOQUEIA A DIGITAÇÃO DE DOIS OPERADORES SEGUIDOS.
             sinal = true
 
             visor.innerHTML += elemento.target.innerHTML
@@ -39,6 +48,7 @@ btn_operador.map((btn_op)=>{
     })
 })
 
+// LIMPA O CONTEÚDO DO VISOR E PASSA A USAR O ARRAY PARA MOSTRAR OS VALORES PARA SER POSSÍVEL APAGAR A INFORMAÇÃO DO VISOR DE UM POR UM COM O USO DO "pop()".
 btn_limpar.addEventListener("click",()=>{
 
     sinal = false
@@ -46,31 +56,45 @@ btn_limpar.addEventListener("click",()=>{
     visor.innerHTML = ""
 
     array_conteudo.pop()
+
+    // TAMBÉM LIMPA O CONTEÚDO DO ARRAY DATA PARA ESTAR DE ACORDO COM O QUE MOSTRA NO VISOR.
     array_data.pop()
     
     array_conteudo.map((ele)=>{
         visor.innerHTML += ele
     })
     
+    // LIMPA A DIV USADA PELO ARRAY DATA E EVAL.
     resultado.innerHTML = ""
 })
 
+// BOTÃO "IGUAL".
 btn_resultado.addEventListener("click",()=>{
 
+    // VERIFICA SE HÁ ALGO DIGITADO NO VISOR.
     if(array_data.length == 0){
         alert("Sem valor para calcular.")
+
     }else{
+
+        // SEMPRE QUE O BOTÃO FOZ CLICADO, LIMPA O ARRAY CONTEUDO PARA NÃO MOSTRAR NADA A MAIS NA TELA QUE NÃO SEJA O RESULTADO.
         array_conteudo = []
     
+        // FAZ A INSERÇÃO DOS ELEMENTOS DO ARRAY DATA NA DIV RESULTADO.
         array_data.map((el)=>{
             resultado.innerHTML += el
         })
         
+        // MOSTRA NO VISOR O RESULTADO DO EVAL QUE USOU A DIV RESULTADO PARA PODER EFETUAR O CÁLCULO.
         visor.innerHTML = eval(resultado.innerHTML)
     
+        // LIMPA O ARRAY DATA.
         array_data = []
+
+        // FAZ UM PUSH DO RESULTADO NO ARRAY DATA PARA SER POSSÍVEL EFETUAR CÁLCULOS COM O VALOR DO RESULTADO.
         array_data.push(visor.innerHTML)
     
+        // LIMPA A DIV USADA PELO ARRAY DATA E EVAL.
         resultado.innerHTML = ""
     }
 })
